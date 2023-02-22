0c92e59a4f7da8be0a6d13f6072f9d9fc836be03
ChanzV <83199396+ChanzV@users.noreply.github.com>
2023-02-22 18:53:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ let peerConnection;
 const servers = {
     iceServers: [
         {
-            urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302','stun:stun.gmx.net']
+            urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302','stun:stun.gmx.net','turn:43.136.35.196[chanzv:123]']
         }
     ]
 }
