6fcc0738deb198290c2ca090a42f77d0a6919ff3
ChanzV <83199396+ChanzV@users.noreply.github.com>
2023-02-24 16:09:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let APP_ID = "c1cb3ea9ce334d8fa80a4f27fd13403e"
 
 
-let token = null;
+//登录rtmClient实例的id，其实就是memberid
 let uid = String(Math.floor(Math.random() * 10000))
-
+console.log('本页面的memberid是', uid);
 let client;
 let channel;
 
@@ -22,44 +22,63 @@ if (!roomId) {
 let localStream;
 let remoteStream;
 let peerConnection;
+let candidateContainer = []
 
 const servers = {
     iceServers: [
         {
-            urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302','stun:stun.gmx.net']
-        },
-        {
             urls:'turn:43.136.35.196',
             username:'chanzv',
             credential:'123'
-        }
+        },
+        // {
+        //     urls: 'stun:stun1.l.google.com:19302'
+        // },
+        // {
+        //     urls: 'stun:stun.qq.com'
+        // }
     ]
 }
 
 
-
+//创建信令实例、频道，并创建成员，进入频道
 let init = async () => {
     //调用createInstance方法，创建并返回一个 RtmClient 实例。
     client = await AgoraRTM.createInstance(APP_ID)
-    await client.login({ uid, token })
+    await client.login({ uid })
 
     //该方法创建一个 RtmChannel 实例
     channel = client.createChannel(roomId)
-    console.log('RtmChannel:', channel);
-
-    //调用该方法加入该频道，加入频道成功后可收到该频道消息和频道用户进退通知。最多可以加入 20 个频道。
-    await channel.join()
 
     //RtmChannel 实例上的事件类型,参考网址如下
     //https://docs.agora.io/cn/Real-time-Messaging/API%20Reference/RTM_web/interfaces/rtmevents.rtmchannelevents.html
     //在这个接口，函数属性的名称为事件名称，函数的参数为事件监听回调的传入参数
+    //类似then(memberid=>{})
+    //memberID出处！这个 handleUserJoined 函数的第一个形参，就是memberID！！！！
     channel.on('MemberJoined', handleUserJoined)
-    channel.on('MemberLeft', handleUserLeft)
 
+    channel.on('MemberLeft', handleUserLeft)
     client.on('MessageFromPeer', handleMessageFromPeer)
 
-    
-    
+    //调用该方法加入该频道，加入频道成功后可收到该频道消息和频道用户进退通知。最多可以加入 20 个频道。
+    await channel.join()
+
+
+
+
+
+}
+let handleUserJoined = async (MemberId) => {
+    console.log('A new user joined the channel:', MemberId)
+    // localStream = await navigator.mediaDevices.getUserMedia(constraints)
+    localStream = await navigator.mediaDevices.getDisplayMedia({
+        video: {
+            width: { ideal: 1920, max: 1920 },
+            height: { ideal: 1080, max: 1080 },
+        }
+    })
+    user1.srcObject = localStream
+    createOffer(MemberId)
 }
 
 
@@ -84,14 +103,18 @@ let handleMessageFromPeer = async (message, MemberId) => {
         console.log(message.answer)
         addAnswer(message.answer)
     }
-
+    
     if (message.type === 'candidate') {
+        console.log('收到了对端传来的candidate')
         if (peerConnection) {
-            peerConnection.addIceCandidate(message.candidate)
+            peerConnection.addIceCandidate(message.candidate).catch(e=>console.log(e))
+            console.log('添加ice候选');
         }
+        else {
+            candidateContainer.push(message.candidate)
+        }
+        console.log(candidateContainer)
     }
-
-
 }
 
 
@@ -100,6 +123,7 @@ let createPeerConnection = async (MemberId) => {
     peerConnection = new RTCPeerConnection(servers)
     console.log('创建了rtc实例');
     console.log(peerConnection);
+    
 
     //返回新建的 MediaStream 实例，该实例作为媒体流的内容的集合载体，其可能包含多个媒体数据轨
     //每个数据轨则由一个 MediaStreamTrack 对象表示。如果给出相应参数，在指定的数据轨则被添加到新的流中。否则，该流中不包含任何数据轨。
@@ -111,31 +135,6 @@ let createPeerConnection = async (MemberId) => {
     user2.style.display = 'block'
 
     user1.classList.add('smallFrame')
-
-
-    // if (caller) {
-    //     //Navigator 接口表示用户代理的状态和标识。它允许脚本查询它和注册自己进行一些活动
-    //     //MediaDevices.getUserMedia() 会提示用户给予使用媒体输入的许可
-    //     //媒体输入会产生一个MediaStream，里面包含了请求的媒体类型的轨道
-    //     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    //         .then(stream => {
-    //             console.log('获取到的stream如下')
-    //             console.log(stream)
-    //             localStream = stream
-    //         })
-    //         .catch(err => console.log(err))
-    //     user1.srcObject = localStream
-    // }
-    // if (!caller) {
-    //     navigator.mediaDevices.getDisplayMedia()
-    //         .then(stream => {
-    //             console.log('获取到的stream如下')
-    //             console.log(stream)
-    //             localStream = stream
-    //         })
-    //         .catch(err => console.log(err))
-    //     user1.srcObject = localStream
-    // }
 
     //将本地流的每个数据轨道都添加到rtcpeerconnection实例中
     console.log('Localstream如下');
@@ -156,6 +155,7 @@ let createPeerConnection = async (MemberId) => {
     peerConnection.onicecandidate = async (event) => {
         console.log('icecandidate事件触发了');
         if (event.candidate) {
+            console.log('发送candidate到' + MemberId)
             client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'candidate', 'candidate': event.candidate }) }, MemberId)
         }
     }
@@ -176,32 +176,25 @@ let constraints = {
     audio: true
 }
 
-let handleUserJoined = async (MemberId) => {
-    console.log('A new user joined the channel:', MemberId)
-    console.log(this);
-    // localStream = await navigator.mediaDevices.getUserMedia(constraints)
-    localStream = await navigator.mediaDevices.getDisplayMedia({
-        video: {
-            width: { ideal: 1920, max: 1920 },
-            height: {ideal: 1080, max: 1080 },
-        }
-    })
-    user1.srcObject = localStream
-    createOffer(MemberId)
-}
+
 
 
 
 //有新成员加入频道时触发
 //创建rtc实例 => 创建并存储offer => 把offer发送到对应的member => member端储存offer并生成answer
 let createOffer = async (MemberId) => {
-    console.log('触发createOffer函数')
     await createPeerConnection(MemberId)
-
     let offer = await peerConnection.createOffer()
-    await peerConnection.setLocalDescription(offer)
-    console.log('已存储本地生成的offer');
-
+    console.log('生成的offer是:'+offer);
+    try {
+        await peerConnection.setLocalDescription(offer)
+    }
+    catch (err) {
+        console.log('设置本地描述失败')
+        console.log(err)
+    }
+    console.log('呼叫方生成并储存offer');
+    console.log('offer发送至',MemberId);
     client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'offer', 'offer': offer }) }, MemberId)
 }
 
@@ -211,29 +204,38 @@ let addAnswer = async (answer) => {
         peerConnection.setRemoteDescription(answer)
         console.log('呼叫方添加setRemoteDescription了');
     }
+    
 }
 
 
 
-/***************被呼叫方****************/ 
-/***************被呼叫方****************/ 
-/***************被呼叫方****************/ 
-/***************被呼叫方****************/ 
-/***************被呼叫方****************/ 
+/***************被呼叫方****************/
+/***************被呼叫方****************/
+/***************被呼叫方****************/
+/***************被呼叫方****************/
+/***************被呼叫方****************/
 
 let createAnswer = async (MemberId, offer) => {
     localStream = await navigator.mediaDevices.getDisplayMedia({
         video: {
             width: { ideal: 1920, max: 1920 },
-            height: {ideal: 1080, max: 1080 },
+            height: { ideal: 1080, max: 1080 },
         }
     })
     user1.srcObject = localStream
-    console.log('触发createAnswer函数');
     await createPeerConnection(MemberId)
 
     await peerConnection.setRemoteDescription(offer)
-    console.log('我真的添加offer到peerconnection了')
+
+    //##################################
+    //标记一下，peerB收到candidate再创造rtc实例的，以后要改
+    if(candidateContainer.length){
+        for(let i = 0;i<candidateContainer.length;i++){
+            peerConnection.addIceCandidate(candidateContainer[i])
+            console.log('添加ice候选');
+        }
+    }
+    console.log('已添加远程描述')
 
     let answer = await peerConnection.createAnswer()
     await peerConnection.setLocalDescription(answer)
